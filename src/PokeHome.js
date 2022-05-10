9629f9655d6f8d73b90037b3933bea15ae3aec87
Kevin Roney <ktroney.pdx@gmail.com>
2022-05-10 16:11:32 -0700

    completed PokeHome time for PokeList

--- src/PokeHome.js
+++ src/PokeHome.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import { getPokemon } from './services/fetch-utils';
+import PokeList from './PokeList';
 
-export default function PokeHome() {
+
+export default function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [page, setPage] = useState(1);
+  const perPage = 20;
+
+  useEffect(() => {
+    async function fetch() {
+      const from = page * perPage - perPage;
+      const to = page * perPage - 1;
+      const pokeData = await getPokemon(from, to);
+
+      setPokemon(pokeData);
+    }
+    fetch();
+  }, [page]);
+  
   return (
-    <div>PokeHome</div>
+    <>
+      <h2>Current Page {page}</h2>
+      <div className='buttons'>
+        <button disabled={page <= 1} onClick={() => setPage(page - 1)}>Previous Page</button>
+        <button disabled={page >= 53} onClick={() => setPage(page + 1)}>Next Page</button>
+      </div>
+      <PokeList pokemon={pokemon}/>
+    </>
   );
 }
---
+++ src/PokeList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function PokeList() {
+  return (
+    <div>PokeList</div>
+  );
+}
