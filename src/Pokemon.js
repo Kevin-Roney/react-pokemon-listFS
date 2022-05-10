19f2db58493909282eca18efe0146b66d0eefd70
Kevin Roney <ktroney.pdx@gmail.com>
2022-05-10 16:24:15 -0700

    completed Pokemon component, all pokemon displaying correctly on PokeHome page

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function Pokemon() {
+export default function Pokemon({ pokemon }) {
   return (
-    <div>Pokemon</div>
+    <Link className='pokemon-list-item' to={`/pokemon/${pokemon.id}`}>
+      <h4>{pokemon.name}</h4>
+      <p>Pokedex Number: {pokemon.id}</p>
+      <p>Type: {pokemon.type1} & {pokemon.type2}</p>
+    </Link>
   );
 }
