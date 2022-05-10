7791101a525e911b4734e54e5afd6a51476b1fa3
Kevin Roney <ktroney.pdx@gmail.com>
2022-05-10 15:38:24 -0700

    added supabase url and key to .env

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
+import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from 'react-router-dom';
+import PokeHome from './PokeHome';
+import PokeDetail from './PokeDetail';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
- 
+        <Switch>
+          <Route exact path='/'>
+            <PokeHome />
+          </Route>
+          <Route path='/pokemon/:id'>
+            <PokeDetail />
+          </Route>
+        </Switch>
       </div>
     </Router>
   );
