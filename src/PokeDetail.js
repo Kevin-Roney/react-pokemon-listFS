b22a20736924a5df06f0d15455c8b98cec012175
Kevin Roney <ktroney.pdx@gmail.com>
2022-05-10 16:29:21 -0700

    completed setting state and useEffect for PokeDetail

--- src/PokeDetail.js
+++ src/PokeDetail.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { getSinglePokemon } from './services/fetch-utils';
 
 export default function PokeDetail() {
+  const [pokemon, setPokemon] = useState({});
+  const params = useParams();
+
+  useEffect(() => {
+    async function fetch() {
+      const pokemonData = getSinglePokemon(params.id);
+      setPokemon(pokemonData);
+    }
+    fetch();
+  }, [params.id]);
   return (
     <div>PokeDetail</div>
   );
