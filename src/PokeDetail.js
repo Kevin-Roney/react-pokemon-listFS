590e3c1c34bea3edb50fb66835c842bb90e6c229
Kevin Roney <ktroney.pdx@gmail.com>
2022-05-10 16:55:04 -0700

    completed elements for PokeDetail page, all displaying correctly, time for css

--- src/PokeDetail.js
+++ src/PokeDetail.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import { getSinglePokemon } from './services/fetch-utils';
+import { getSinglePokemon } from './services/fetch-utils.js';
 
 export default function PokeDetail() {
   const [pokemon, setPokemon] = useState({});
@@ -10,12 +10,26 @@ export default function PokeDetail() {
 
   useEffect(() => {
     async function fetch() {
-      const pokemonData = getSinglePokemon(params.id);
+      const pokemonData = await getSinglePokemon(params.id);
+
       setPokemon(pokemonData);
     }
     fetch();
   }, [params.id]);
   return (
-    <div>PokeDetail</div>
+    <>
+      <Link to='/'>Home</Link>
+      <div className='poke-detail'>
+        <h2>{pokemon.name}</h2>
+        <p>Is pokedex number: {pokemon.id}</p>
+        <p>Type: {pokemon.type1} & {pokemon.type2}</p>
+        <p>HP: {pokemon.hp}</p>
+        <p>Attack: {pokemon.attack}</p>
+        <p>Defense: {pokemon.defense}</p>
+        <p>Sp. Attack: {pokemon.sp_attack}</p>
+        <p>Sp. Defense: {pokemon.sp_defense}</p>
+        <p>Speed: {pokemon.speed}</p>
+      </div>
+    </>
   );
 }
